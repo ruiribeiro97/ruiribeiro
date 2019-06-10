70161e8c348a12dd98cbf8f1b32c0a38bdc3977e
Rui Ribeiro [1150344] <1150344@isep.ipp.pt>
2019-06-10 19:53:46 +0100

    foda-se corga

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,31 @@ import 'hammerjs';
 })
 export class AppComponent {
   title = 'Rui Ribeiro';
+  config: any;
+  fullpage_api: any;
+
+  constructor() {
+    this.config = {
+
+      // fullpage options
+      licenseKey: null,
+      anchors: ['home', 'about', 'projects', 'contact'],
+      menu: '#menu',
+      lockAnchors: true,
+
+      // fullpage callbacks
+      afterResize: () => {
+        // console.log("After resize");
+      },
+      afterLoad: (origin, destination, direction) => {
+        // console.log(origin.index);
+      }
+    };
+  }
+
+  getRef(fullPageRef) {
+    this.fullpage_api = fullPageRef;
+  }
 
   getState(outlet: { activatedRouteData: { state: any; }; }) {
     return outlet.activatedRouteData.state;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,6 +35,7 @@ import { GithubComponent } from './components/projects/github/github.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularFullpageModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FlexLayoutModule,
